530ba2a71141b8484303037c68fab784b99b8d4b
Thomas Moon <mail@thomasmoon.net>
2020-05-13 05:26:23 +0300

    Reorganize imports on main app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,16 @@
+// Environment
+import { environment } from '../environments/environment';
+
+// Angular
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MapComponent } from './course/map/map.component';
-import { Dialog } from './dialog/dialog.component';
-
-import { MapService } from './services/map.service';
-import { SidenavService } from './services/sidenav.service';
-import { CourseSidenavService } from './services/coursesidenav.service';
-
-import { environment } from '../environments/environment';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-
-import { AuthService } from './services/auth.service';
-import { AuthGuard } from './services/auth.guard';
-import { AdminGuard } from './services/admin.guard';
-
-import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
-
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule } from '@angular/material/list';
+// Material Design
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -41,19 +26,51 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
+
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
+
+// Routes
+import { AppRoutingModule } from './app-routing.module';
+
+// Auth Service & Guards
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth.guard';
+import { AdminGuard } from './services/admin.guard';
+
+// Other Services
+import { MapService } from './services/map.service';
+import { SidenavService } from './services/sidenav.service';
+import { CourseSidenavService } from './services/coursesidenav.service';
+
+// Components
+import { AppComponent } from './app.component';
+
+// Shared Components
+import { TableComponent } from './shared/table/table.component';
+import { Dialog } from './dialog/dialog.component';
 import { MediaToolsComponent } from './media-tools/media-tools.component';
+
+// Course Components
+import { CourseComponent } from './course/course.component';
+import { MapComponent } from './course/map/map.component';
 import { ContentComponent } from './course/content/content.component';
 import { ToolsComponent } from './course/tools/tools.component';
-import { CourseComponent } from './course/course.component';
 import { VideoComponent } from './course/video/video.component';
 import { CommentsComponent } from './course/comments/comments.component';
+
+// Admin Components
 import { VideosComponent } from './admin/videos/videos.component';
 import { CommentsAdminComponent } from './admin/comments/comments.component';
-import { TableComponent } from './shared/table/table.component';
+
+// Other Components
 import { PrivacyComponent } from './privacy/privacy.component';
 import { LessonComponent } from './lesson/lesson.component';
 
-
+// Application Module
 @NgModule({
   declarations: [
     AppComponent,
